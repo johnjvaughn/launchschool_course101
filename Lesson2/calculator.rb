1af892b1fc50bbac8273017d121d50c3abfa215e
John Vaughn <johnjvaughn@gmail.com>
2017-08-16 11:41:24 -0400

    Lesson2: calculator program

---
+++ Lesson2/calculator.rb
@@ -0,0 +1,23 @@
+puts("Enter two numbers separated by a space:")
+numbers = gets.chomp().split()
+numbers.map! { |num| num.to_i() }
+puts("Perform which operation? Type + to add, - to subtract, * to multiply, or / to divide.")
+operation = gets.chomp()
+result = case operation
+when '+'
+  numbers[0] + numbers[1]
+when '-'
+  numbers[0] - numbers[1]
+when '*'
+  numbers[0] * numbers[1]
+when '/'
+  if numbers[1] == 0
+    "Error, cannot divide by zero."
+  else
+    numbers[0].to_f / numbers[1]
+  end
+else
+  "Error, unknown operation."
+end
+
+puts("Answer: #{result}")
