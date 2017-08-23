9212f3e3b6b67c79e85764c9c86b41f149f7837b
John Vaughn <johnjvaughn@gmail.com>
2017-08-23 16:18:41 -0400

    Twentyone changes after code review by Dalton

--- Lesson6/twentyone.rb
+++ Lesson6/twentyone.rb
@@ -1,5 +1,4 @@
-# rubocop:disable Style/EndOfLine
-require('pry')
+require 'pry'
 
 RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 SUITS = ['C', 'D', 'H', 'S']
@@ -12,7 +11,24 @@ def prompt(message)
 end
 
 def clear_screen
-  system('clear') or system('cls')
+  system('clear') || system('cls')
+end
+
+def user_choice(question, choices, accept_first_letter=true)
+  choice_hash = {}
+  choices.each { |choice| choice_hash[choice.downcase] = choice.downcase[0] }
+  choices_out = accept_first_letter ? choices.map(&:capitalize) : choices
+
+  prompt_line = "#{question} (#{choices_out.join('/')})"
+  loop do
+    prompt prompt_line
+    answer = gets.strip.downcase
+    if choice_hash.key?(answer)
+      return answer
+    elsif accept_first_letter && choice_hash.value?(answer)
+      return choice_hash.key(answer)
+    end
+  end
 end
 
 def total(cards)
@@ -57,6 +73,7 @@ def deal_another_card!(deck, hand)
 end
 
 def show_hand(cards, player, total=0)
+  puts
   if total > 0
     output = cards.map { |card| card.reverse.join }.join(', ')
     prompt "#{player.capitalize} has: #{output} (total = #{total})"
@@ -70,27 +87,34 @@ def busted?(total)
   total > BUST_IF_OVER
 end
 
-def decide_winner_and_display(player_total, dealer_total)
-  # this generates a rubocop warning about too many lines
+def decide_winner(player_total, dealer_total)
+  if busted?(player_total)
+    :dealer
+  elsif busted?(dealer_total)
+    :player
+  elsif player_total > dealer_total
+    :player
+  elsif dealer_total > player_total
+    :dealer
+  end
+end
+
+def display_winner(player_total, dealer_total)
+  puts
   puts "==============================="
   if busted?(player_total)
     prompt "Dealer wins due to Player BUST!"
-    winner = :dealer
   elsif busted?(dealer_total)
     prompt "Player wins due to Dealer BUST!"
-    winner = :player
   elsif player_total > dealer_total
     prompt "Player wins, #{player_total} to #{dealer_total}!"
-    winner = :player
   elsif dealer_total > player_total
     prompt "Dealer wins, #{dealer_total} to #{player_total}!"
-    winner = :dealer
   else
     prompt "It's a tie, #{player_total} each."
-    winner = nil
   end
   puts "==============================="
-  winner
+  puts
 end
 
 def display_match_score(score)
@@ -108,7 +132,7 @@ def check_for_a_match_winner(score)
       return competitor.capitalize
     end
   end
-  false
+  nil
 end
 
 def display_match_results(winner)
@@ -130,23 +154,15 @@ loop do
   dealer_hand = []
 
   deal_new_hand!(deck, player_hand, dealer_hand)
-  puts
   dealer_total = total(dealer_hand)
   show_hand(dealer_hand, :dealer)
-  puts
   player_total = total(player_hand)
   show_hand(player_hand, :player, player_total)
   winner = nil
 
   loop do
-    break if player_total == BUST_IF_OVER
-    prompt "(H)IT or (S)TAY?"
-    answer = gets.downcase
-    if answer.start_with?('s')
-      break
-    elsif !answer.start_with?('h')
-      next
-    end
+    break if player_total == BUST_IF_OVER # exactly 21, no need to ask
+    break if user_choice("What's your action?", ['hit', 'stay']) == 'stay'
     deal_another_card!(deck, player_hand)
     player_total = total(player_hand)
     show_hand(player_hand, :player, player_total)
@@ -157,7 +173,6 @@ loop do
     end
   end
 
-  puts
   show_hand(dealer_hand, :dealer, dealer_total)
 
   unless winner
@@ -183,21 +198,18 @@ loop do
     end
   end
 
-  puts
-  winner = decide_winner_and_display(player_total, dealer_total)
-  puts
-
+  display_winner(player_total, dealer_total)
+  winner = decide_winner(player_total, dealer_total)
   match_score[winner] += 1 if winner
   display_match_score(match_score)
   match_winner = check_for_a_match_winner(match_score)
   if match_winner
     display_match_results(match_winner)
     match_score = initialize_score
-    prompt "Start a new match? ([y]/n)"
-  else
-    prompt "Play again? ([y]/n)"
+    break if user_choice("Start a new match?", ['yes', 'no']) == 'no'
+  elsif user_choice("Play again?", ['yes', 'no']) == 'no'
+    break
   end
-  break if gets.downcase.start_with?('n')
   clear_screen
 end
 
